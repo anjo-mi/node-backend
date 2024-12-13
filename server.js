b8b792113f1d923221be9adb35ee54fab019e9e9
kyle <nojaim.k@gmail.com>
2024-12-13 14:31:15 -0500

    server runs and loads styles, main js is next

---
+++ server.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const queryString = require('querystring');
+
+const server = http.createServer((req,res) => {
+    const page = url.parse(req.url).pathname;
+    const params = queryString.parse(url.parse(req.url).query);
+
+    if (page === '/'){
+        fs.readFile('index.html', (err,data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        })
+    }
+
+    else if (page === '/css/style.css'){
+        fs.readFile('css/style.css', (err,data) => {
+            res.write(data);
+            res.end();
+        })
+    }
+    else if( page === '/js/main.js'){
+        fs.readFile('js/main.js', (err,data) => {
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.write(data);
+            res.end();
+        })
+    }
+
+    else if (page === '/images/jcole.jpg'){
+        fs.readFile('images/jcole.jpg', (err,data) => {
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (page === '/images/kendrick.jpg'){
+        fs.readFile('images/kendrick.jpg', (err,data) => {
+            res.write(data);
+            res.end();
+        })
+    }
+
+    else if (page === '/images/drake.webp'){
+        fs.readFile('images/drake.webp', (err,data) => {
+            res.write(data);
+            res.end();
+        })
+    }
+})
+
+server.listen(9000);

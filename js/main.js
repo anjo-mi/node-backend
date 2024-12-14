b724dff94e3da0b216a3349ddf8899a195e53ae2
kyle <nojaim.k@gmail.com>
2024-12-14 00:01:11 -0500

    changing links hrhref works, trying to load landing

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ document.querySelectorAll('.rapper').forEach(rapper => {
 })
 
 async function makeChoice(e){
-    console.log('clicked')
     const artist = e.target.getAttribute('artist');
-    e.preventDefault();
     console.log(artist);
 
     const response = await fetch(`/api?artist=${artist}`);
     const data = await response.json();
-
     console.log(data)
+
+    const link = document.querySelector('.clickToSee');
+    const anchor = link.querySelector('.link');
+
+    anchor.href = data.src
+
+    link.style.display = 'block';
 }
